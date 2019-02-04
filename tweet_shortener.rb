409f579252a1c3ed87239efdb72a4b4aacc3b168
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 20:20:29 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,70 @@
+require 'pry'
 # Write your code here.
+def dictionary
+  hash = {'hello' => 'hi', 'to' => '2', 'two' => '2', 'too' => '2', 'for' => '4', 'four' => '4', 'be' => 'b', 'you' => 'u', "at" => "@", "and" => "&"}
+end
+
+def word_substituter(string)
+  words_to_replace = dictionary.keys
+  array1 = string.split(" ")
+
+  array1.each do |element|
+    if words_to_replace.include?(element)
+      element.replace(dictionary.fetch(element))
+    end
+  end
+  return array1.join(" ")
+end
+
+def dictionary
+  hash = {'hello' => 'hi', 'to' => '2', 'two' => '2', 'too' => '2', 'for' => '4', 'four' => '4', 'be' => 'b', 'you' => 'u', "at" => "@", "and" => "&"}
+end
+
+def bulk_tweet_shortener(array)
+  words_to_replace = dictionary.keys
+
+  array.each do |tweet|
+    array1 = tweet.split(" ")
+
+   array1.each do |element|
+      if words_to_replace.include?(element.downcase)
+        element.replace(dictionary.fetch(element.downcase))
+      end
+    end
+    puts array1.join(" ")
+  end
+end
+
+
+def selective_tweet_shortener(array)
+  words_to_replace = dictionary.keys
+
+  array.each do |tweet|
+    array1 = tweet.split(" ")
+    if array1.size > 140
+   array1.each do |element|
+      if words_to_replace.include?(element.downcase)
+        element.replace(dictionary.fetch(element.downcase))
+      end
+    end
+  end
+    puts array1.join(" ")
+  end
+end
+
+
+def selective_tweet_shortener(string)
+  if string.size > 140
+    word_substituter(string)
+  else
+    return string
+  end
+end
+
+def shortened_tweet_truncator
+  if string.size > 140
+    word_substituter(string)
+  else
+    return string.truncate(140) + "..."
+  end
+end
