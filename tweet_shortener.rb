7a7e598632b7e2b9965ad139c4357cf5fe0da16d
Anders Hornor <anders.hornor@gmail.com>
2019-02-04 17:23:12 -0800

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require 'pry'
+def dictionary
+  { "hello" => 'hi', "to" => '2', "two" => '2', "too" => '2', "for" => '4', "four" => "4", 'be' => 'b',
+'you' => 'u', "at" => "@", "and" => "&" }
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  dictionary_array = dictionary.keys
+  n = 0
+  tweet_array.each do |n|
+    == dictionary_array[n]
+    n=+1
+    binding.pry
+  end
+end
